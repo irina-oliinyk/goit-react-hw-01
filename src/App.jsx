d5d8bf2ee760a6bab80a4e83bdd507026ea5af6a
Iryna Oliinyk <arairishka2@gmail.com>
2024-08-10 23:28:29 +0200

    task3

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@
 import './App.css';
 import Profile from './components/Profile/Profile';
 import FrendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import userData from './data/userData.json';
 import friend from './data/friends.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
     <>
       <Profile profile={userData} />
       <FrendList friends={friend} />
+      <TransactionHistory item={transactions} />
     </>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,24 @@
+import css from './TransactionHistory.module.css';
+
+export default function TransactionHistory({ item }) {
+  return (
+    <table className={css.table}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {item.map(({ type, amount, currency, id }) => (
+          <tr key={id} className={css.tr}>
+            <td className={css.td}>{type}</td>
+            <td className={css.td}>{amount}</td>
+            <td className={css.td}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
